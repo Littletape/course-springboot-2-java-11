6aa96a0916c811ae112515afaeec270f5134b7b7
Littletape <andersoncardosolima0@gmail.com>
2020-07-03 13:23:23 -0300

    OrderStatus enum

--- src/main/java/com/andersoncardosolima/course/entities/Order.java
+++ src/main/java/com/andersoncardosolima/course/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.andersoncardosolima.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity // informa o jpa que isto é uma entidade
@@ -25,6 +26,8 @@ public class Order implements Serializable {
 	// Formatação Json do campo, respeitando o padrão ISO 8601
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne // informa para o JPA relação muitos para um
 	@JoinColumn(name = "client_id") // nome da chave estrangeira no banco de dados
@@ -33,10 +36,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -54,6 +58,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
