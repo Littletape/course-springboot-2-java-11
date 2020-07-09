0bf9aabc8d67c473077f1cdfbedcd151ebd2e6b5
Littletape <andersoncardosolima0@gmail.com>
2020-07-09 11:54:37 -0300

    User insert

--- src/main/java/com/andersoncardosolima/course/resources/UserResource.java
+++ src/main/java/com/andersoncardosolima/course/resources/UserResource.java
@@ -1,34 +1,48 @@
 package com.andersoncardosolima.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andersoncardosolima.course.entities.User;
 import com.andersoncardosolima.course.services.UserService;
 
 @RestController
-@RequestMapping(value="/users") // caminho da url no navegador
+@RequestMapping(value = "/users") // caminho da url no navegador
 public class UserResource {
-	
+
 	@Autowired // anotação Spring para dependencia
 	private UserService service;
-	
-	@GetMapping // indica para o navegador que o metodo é do tipo Get no padrao JSON
-	public ResponseEntity<List<User>> findAll(){
+
+	@GetMapping // indica para o navegador que o metodo é do tipo GET(html) no padrao JSON
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
-		
+
 		return ResponseEntity.ok(list);
 	}
-	
-	@GetMapping(value = "/{id}") //Get recebera id
-	public ResponseEntity<User> findById(@PathVariable Long id){
+
+	@GetMapping(value = "/{id}") // Get recebera id
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findbyId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping // indica para o navegador que o metodo é do tipo POST(html) no padrao JSON
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		
+		// nessessario para retornar resposta 201 (Created new resource) ao enviar o POST pelo Json 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
