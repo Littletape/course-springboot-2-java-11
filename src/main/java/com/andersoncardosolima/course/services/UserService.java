f37a1d63415502d27a9c7922dc7e6374902cae21
Littletape <andersoncardosolima0@gmail.com>
2020-07-10 14:37:51 -0300

    User update

--- src/main/java/com/andersoncardosolima/course/services/UserService.java
+++ src/main/java/com/andersoncardosolima/course/services/UserService.java
@@ -29,8 +29,20 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id); // prepara obj monitorado pelo jpa antes de acessar o banco
+		updateData(entity, obj);
+		return repository.save(entity); // salva no banco
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
