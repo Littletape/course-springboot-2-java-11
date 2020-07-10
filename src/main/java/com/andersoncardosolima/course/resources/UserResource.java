001e5be13a5598ed35b30342f1348a84ad8e652c
Littletape <andersoncardosolima0@gmail.com>
2020-07-10 14:16:48 -0300

    User delete

--- src/main/java/com/andersoncardosolima/course/resources/UserResource.java
+++ src/main/java/com/andersoncardosolima/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,10 +40,17 @@ public class UserResource {
 	@PostMapping // indica para o navegador que o metodo é do tipo POST(html) no padrao JSON
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		
-		// nessessario para retornar resposta 201 (Created new resource) ao enviar o POST pelo Json 
+
+		// nessessario para retornar resposta 201 (Created new resource) ao enviar o
+		// POST pelo Json
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
